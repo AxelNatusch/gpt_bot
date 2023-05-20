4bf56bf0bbb7346e455d9a1baf717869f41a8bc7
Axel Natusch <eslbaum5@gmail.com>
2023-05-20 20:59:22 +0200

    testing ci/cd

--- bot-1/app.py
+++ bot-1/app.py
@@ -20,7 +20,7 @@ def get_system_prompt(scenario):
     elif scenario == "5":
         return chatbot_scenarios.markdown_beautifier
     elif scenario == "6":
-        return "placeholder2"
+        return "placeholder"
 
 
 @app.route("/")
